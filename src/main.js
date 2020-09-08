40961eea096ede693c9b70d4c066586544f071f6
V1codin <blindmonk46@gmail.com>
2020-09-08 18:47:46 +0300

    added header and main

--- src/main.js
+++ src/main.js
@@ -8,14 +8,13 @@ $(".showcase__slider").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   speed: 500,
-  fade: true,
-  cssEase: "linear",
   adaptiveHeight: true,
-  initialSlide: 1,
+  cssEase: "linear",
+  fade: true,
   swipe: true,
-  variableWidth: true,
+  initialSlide: 1,
 });
 
-document.querySelector(".form__btn").onclick = (e) => {
+document.querySelector(".form__btn").onclick = function (e) {
   e.preventDefault();
 };
